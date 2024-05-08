43564cce9515c80194dac7e40acaf7d094f1409b
mathiamt <mtyseng@gmail.com>
2024-05-08 13:50:38 -0700

    mqtt to queue songs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 import uvicorn
@@ -12,9 +12,6 @@ import os
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
-client1 = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="", userdata=None, protocol=paho.MQTTv5)
-
-load_dotenv()  # Load environment variables from .env file
 
 
 
@@ -33,15 +30,36 @@ def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None):
+    """
+        Prints mid to stdout to reassure a successful publish ( used as callback for publish )
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param mid: variable returned from the corresponding publish() call, to allow outgoing messages to be tracked
+        :param properties: can be used in MQTTv5, but is optional
+    """
+    print("mid: " + str(mid))
 
-client1.on_connect = on_connect
-# enable TLS for secure connection
-client1.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-# set username and password
-client1.username_pw_set(os.getenv("HIVEMQ_USERNAME"), os.getenv("HIVEMQ_PASSWORD"))
-# connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client1.connect("6e55730bd5364136b3eea32f4fcb7183.s1.eu.hivemq.cloud", 8883)
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    """
+        Prints a reassurance for successfully subscribing
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param mid: variable returned from the corresponding publish() call, to allow outgoing messages to be tracked
+        :param granted_qos: this is the qos that you declare when subscribing, use the same one for publishing
+        :param properties: can be used in MQTTv5, but is optional
+    """
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
+def on_message(client, userdata, msg):
+    """
+        Prints a mqtt message to stdout ( used as callback for subscribe )
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param msg: the message with topic and payload
+    """
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 
 
@@ -55,12 +73,54 @@ async def get():
 async def queue_add(request: Request):
     data = await request.json()
     song = data.get("song")
+    print(song)
+    
     if song:
-        client1.publish("queue/add", payload=song, qos=1)
+        client.publish("songs/add", payload=song, qos=1)
         return {"message": "Song added to the queue"}
     else:
         return {"message": "No song provided"}
 
 
+
+
 if __name__ == '__main__':
+    load_dotenv()
+
+    client = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="", userdata=None, protocol=paho.MQTTv5)
+
+    broker_address = os.environ.get('BROKER_ADDRESS')
+    broker_port = int(os.environ.get('BROKER_PORT'))
+    username = os.environ.get('USER_NAME')
+    password = os.environ.get('PASSWORD')
+
+    
+
+
+    # enable TLS for secure connection
+    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    # set username and password
+    client.username_pw_set(username, password)
+    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+    client.connect(broker_address, broker_port)
+
+    client_sub = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="", userdata=None, protocol=paho.MQTTv5)
+    client_sub.on_connect = on_connect
+
+     # enable TLS for secure connection
+    client_sub.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    # set username and password
+    client_sub.username_pw_set(username, password)
+    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+    client_sub.connect(broker_address, broker_port)
+
+    client_sub.on_message = on_message
+    client_sub.on_publish = on_publish
+
+    # subscribe to all topics of numbers by using the wildcard "#"
+    client_sub.subscribe("songs/#", qos=1)
+
+
+    client_sub.loop_start()
+
     uvicorn.run(app)
--- static/queue.js
+++ static/queue.js
@@ -1,46 +1,52 @@
-const queueButton = document.querySelector('.queue-btn');
+document.addEventListener("DOMContentLoaded", () => {    
+    const queueButton = document.getElementById('queue-btn');
+    const searchInput = document.getElementById('search-input');
+    console.log("queueButton")
 
-// Fetch the CSV file
-fetch('static/TopSongs.csv')
-    .then(response => response.text())
-    .then(data => {
-        const musicData = data.split('\n').map(line => line.trim());
-        const datalist = document.getElementById('music-suggestions');
-        
-        // Populate the datalist with music suggestions
-        musicData.forEach(music => {
-            const [_, artist, songName, year] = music.split(',');
-            if (artist && songName && year) {
-                const option = document.createElement('option');
-                option.value = `${artist} - ${songName} (${year})`;
-                datalist.appendChild(option);
-            }
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-
-
-
-queueButton.addEventListener('click', function() {
-    const selectedSong = searchBar.value;
-    if (selectedSong) {
-        fetch('/queue_add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ song: selectedSong })
-        })
-        .then(response => response.json())
+    // Fetch the CSV file
+    fetch('static/TopSongs.csv')
+        .then(response => response.text())
         .then(data => {
-            console.log(data.message);
-            // Optionally, you can display a success message to the user
+            const musicData = data.split('\n').map(line => line.trim());
+            const datalist = document.getElementById('music-suggestions');
+            
+            // Populate the datalist with music suggestions
+            musicData.forEach(music => {
+                const [_, artist, songName, year] = music.split(',');
+                if (artist && songName && year) {
+                    const option = document.createElement('option');
+                    option.value = `${artist} - ${songName} (${year})`;
+                    datalist.appendChild(option);
+                }
+            });
         })
         .catch(error => {
             console.error('Error:', error);
-            // Optionally, you can display an error message to the user
         });
-    }
+
+
+
+    queueButton.addEventListener('click', function() {
+        const selectedSong = searchInput.value;
+        console.log(selectedSong)
+        if (selectedSong) {
+            fetch('/queue_add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ song: selectedSong })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message);
+                // Optionally, you can display a success message to the user
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Optionally, you can display an error message to the user
+            });
+        }
+    });
+
 });
